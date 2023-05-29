d88d90577a3ed5f52d7b9b4e7a429b7558aae1fd
Ariuka69 <122322394+Ariuka69@users.noreply.github.com>
2023-05-29 17:57:43 +0800

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,22 @@
+# Specify the file path to the new wallpaper image
+$wallpaperPath = "$HOME" + "\Downloads\troll-face-meme.jpg"
+
+# Set the wallpaper using the SystemParametersInfo function from user32.dll
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class WallpaperChanger {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+
+"@
+
+# Constants for the wallpaper change action
+$SPI_SETDESKWALLPAPER = 0x0014
+$SPIF_UPDATEINIFILE = 0x01
+$SPIF_SENDCHANGE = 0x02
+
+# Call the SystemParametersInfo function to change the wallpaper
+[WallpaperChanger]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $wallpaperPath, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
