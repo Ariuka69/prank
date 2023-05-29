1aacf2d9b37e3937ddeec45ad8f1cbea243eef74
Ariuka69 <122322394+Ariuka69@users.noreply.github.com>
2023-05-29 22:07:53 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 # Specify the file path to the new wallpaper image
-$wallpaperPath = "$HOME" + "\troll-face-meme.jpg"
+$wallpaperPath = "$HOME" + "Downloads\troll-face-meme.jpg"
 
 # Set the wallpaper using the SystemParametersInfo function from user32.dll
 Add-Type -TypeDefinition @"
